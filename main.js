1ef5274f09ab81518696e7a3feb6cd3544fcfa85
azimjonxatamov <azimjonxatamov6@gmail.com>
2024-07-20 12:08:43 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let a = +prompt('Birdan 7 gacha son kiriting')
+
+switch (a) {
+    case 1:
+        alert('duyshanbi');
+        break
+
+        case 2:
+        alert('seshanbi');
+        break
+
+        case 3:
+        alert('chorshanbi');
+        break
+
+        case 4:
+        alert('pashanbi');
+        break
+        
+        case 5:
+        alert('juma');
+        break
+
+        case 6:
+        alert('shanbi');
+        break
+
+        case 7:
+        alert('yakshanbi');
+        break
+        default:
+        alert('bir havtada 7 kun bor ')
+                    
+                
+                
+            
+            
+        
+        
+}
